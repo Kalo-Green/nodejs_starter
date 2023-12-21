30d771cf1775ead3e596bf4a67ff9167d3f2011a
Kalo-Green <kalogreenstyle@gmail.com>
2023-12-21 14:40:01 +0100

    Création du serveur

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
 // Import des packages nécessaire
+require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
+
+// Middleware
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Afficher  hello world
+app.get('/', (req, res) => {
+    res.send('Hello World !');
+});
+
+// Lancement du serveur
+const PORT = process.env.PORT || 5600;
+
+// Définir une fonction pour lancer le serveur
+const start = async () => {
+
+    try {
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (error) {
+        console.log(error);
+    }
+};
+start();
